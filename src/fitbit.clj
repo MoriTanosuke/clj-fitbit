150febf2c412114f9f973d0609bd86800d4eb45f
Carsten <carsten.ringe@web.de>
2011-03-15 19:44:59 +0100

    Added some short functions to build parts of URL and URL itself.

--- src/fitbit.clj
+++ src/fitbit.clj
@@ -3,6 +3,7 @@
 (require ['oauth.client :as 'oauth]
          ['fitbit.settings :as 'settings])
 
+;; oauth preparations
 (def consumer (oauth/make-consumer (:consumer_key settings/settings)
                                    (:consumer_secret settings/settings)
                                    "http://api.fitbit.com/oauth/request_token"
@@ -10,52 +11,47 @@
                                    "http://api.fitbit.com/oauth/authorize"
                                    :hmac-sha1))
 
-(def request-token (oauth/request-token consumer))
+(def request-token
+  (oauth/request-token consumer))
 
-(println (oauth/user-approval-uri consumer
-                         request-token))
+(println (oauth/user-approval-uri consumer request-token))
 
 (println "Enter your PIN:")
-(def settings (merge settings/settings {:pin (read-line)}))
+(def settings
+  (merge settings/settings {:pin (read-line)}))
 
-(def access-token-response (oauth/access-token consumer 
-                                               request-token
-                                               (:pin settings)))
+(def access-token-response
+  (oauth/access-token consumer request-token (:pin settings)))
 
+
+;; api access
 (require ['com.twinql.clojure.http :as 'http])
 
-(http/get "http://www.google.de"
-  :parameters (http/map->params {:use-expect-continue false
-                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)}))
+(defn today
+  [] (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (java.util.Date.)))
 
-(http/get "http://www.google.de"
+(defn get-url
+  ([action date] (str "http://api.fitbit.com/1/user/-/" action "/date/" date ".json")))
+
+(println (:content
+           (http/get (get-url "activities" (today))
+             :headers {"Authorization" (oauth/authorization-header
+                                         (oauth/credentials consumer
+                                           (:oauth_token access-token-response)
+                                           (:oauth_token_secret access-token-response)
+                                           :GET
+                                           (get-url "activities" (today))))}
+             :parameters (http/map->params {:use-expect-continue false
+                                            :default-proxy (http/http-host :host "127.0.0.1" :port 8765)})
+             :as :string)))
+
+(println (:content (http/get (get-url "foods/log" (today))
   :headers {"Authorization" (oauth/authorization-header
                               (oauth/credentials consumer
                                 (:oauth_token access-token-response)
                                 (:oauth_token_secret access-token-response)
                                 :GET
-                                "http://www.google.de"))}
+                                (get-url "foods/log" (today))))}
   :parameters (http/map->params {:use-expect-continue false
-                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)}))
-
-
-
-(http/get "http://api.fitbit.com/1/user/-/activities/date/2011-03-14.json"
-  :headers {"Authorization" (oauth/authorization-header
-                              (oauth/credentials consumer
-                                (:oauth_token access-token-response)
-                                (:oauth_token_secret access-token-response)
-                                :GET
-                                "http://api.fitbit.com/1/user/-/activities/date/2011-03-14.json"))}
-  :parameters (http/map->params {:use-expect-continue false
-                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)}))
-
-(http/get "http://api.fitbit.com/1/user/-/foods/log/date/2011-03-14.json"
-  :headers {"Authorization" (oauth/authorization-header
-                              (oauth/credentials consumer
-                                (:oauth_token access-token-response)
-                                (:oauth_token_secret access-token-response)
-                                :GET
-                                "http://api.fitbit.com/1/user/-/foods/log/date/2011-03-14.json"))}
-  :parameters (http/map->params {:use-expect-continue false
-                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)}))
+                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)})
+             :as :string)))
