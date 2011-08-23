cf5402277bb011226c94db5c3514fae58becf811
Carsten Ringe <carsten.ringe@web.de>
2011-08-23 20:18:16 +0200

    Clean up and remove old stuff.
    Need to start over again. I need to wrap my head around using more
    lists and nesting functions. But how to do that for user input like
    the oauth PIN? No idea...

--- src/fitbit.clj
+++ src/fitbit.clj
@@ -1,51 +1,51 @@
 (ns fitbit)
 
 (require ['oauth.client :as 'oauth]
+         ['com.twinql.clojure.http :as 'http]
          ['fitbit.fitbitutil :as 'util]
-         ['fitbit.settings :as 'settings])
+         ['fitbit.settings :as 'settings]
+         ['clojure.contrib.json :as 'json])
 
-;; oauth preparations
 (def consumer (oauth/make-consumer (:consumer_key settings/settings)
                                    (:consumer_secret settings/settings)
                                    "http://api.fitbit.com/oauth/request_token"
                                    "http://api.fitbit.com/oauth/access_token"
-                                   "http://api.fitbit.com/oauth/authorize"
+                                   "http://www.fitbit.com/oauth/authorize"
                                    :hmac-sha1))
 
-(def request-token
-  (oauth/request-token consumer))
+(def settings (merge settings/settings {}))
 
-(println (oauth/user-approval-uri consumer request-token))
-
-(println "Enter your PIN:")
-(def settings (merge settings/settings {:pin (read-line)}))
-
-(def access-token-response
-  (oauth/access-token consumer request-token (:pin settings)))
-
-
-;; api access
-(require ['com.twinql.clojure.http :as 'http])
-
-(def todays-food (:content (http/get (util/get-url "foods/log" (util/today-as-str))
+;TODO check if oauth_token and oauth_token_secret are present in settings
+;;skip if token already set 
+;(def request-token
+;	(oauth/request-token consumer))
+;
+;(println (oauth/user-approval-uri consumer request-token))
+;(println "Enter your PIN:")
+;(def settings (merge settings {:pin (read-line)}))
+;
+;(oauth/user-approval-uri consumer 
+;                         (:oauth_token request-token))
+;
+;(def access-token-response (oauth/access-token consumer 
+;                                               request-token
+;                                               (:pin settings)))
+;
+;(def settings (merge settings access-token-response))
+;TODO write settings to file
+                                               
+(def todays-food (json/read-json
+                   (:content (http/get (util/get-url "foods/log" (util/today-as-str))
   :headers {"Authorization" (oauth/authorization-header
                               (oauth/credentials consumer
-                                (:oauth_token access-token-response)
-                                (:oauth_token_secret access-token-response)
+                                (:oauth_token settings)
+                                (:oauth_token_secret settings)
                                 :GET
                                 (util/get-url "foods/log" (util/today-as-str))))}
   :parameters (http/map->params {:use-expect-continue false
-                                 :default-proxy (http/http-host :host "127.0.0.1" :port 8765)})
-             :as :string)))
+                                 ;:default-proxy (http/http-host :host "127.0.0.1" :port 8765)
+                                 })
+             :as :string))))
 
-(def todays-activities (:content
-            (http/get (util/get-url "activities" (util/today-as-str))
-              :headers {"Authorization" (oauth/authorization-header
-                                          (oauth/credentials consumer
-                                            (:oauth_token access-token-response)
-                                            (:oauth_token_secret access-token-response)
-                                            :GET
-                                            (util/get-url "activities" (util/today-as-str))))}
-              :parameters (http/map->params {:use-expect-continue false
-                                             :default-proxy (http/http-host :host "127.0.0.1" :port 8765)})
-              :as :string)))
+(println todays-food)
+(reduce todays-food println)
--- src/fitbit/settings.clj
+++ src/fitbit/settings.clj
@@ -1,4 +1,7 @@
 (ns fitbit.settings)
 
 (def settings {:consumer_key "247590bdcc364668858dae234f93d1a6",
-               :consumer_secret "a0c466cc38474f8f9332ebc9eb4e5407"})
+               :consumer_secret "a0c466cc38474f8f9332ebc9eb4e5407",
+               :oauth_token "8787be391a2a43351a72426ab39a8a8d",
+               :oauth_token_secret "63917486413033aeecc094124da64cbc"
+               })
